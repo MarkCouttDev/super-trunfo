c46d34a636c0893a976e1c15b61379d73fc684a8
MarkCouttDev <157859363+MarkCouttDev@users.noreply.github.com>
2025-07-22 08:30:37 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -44,6 +44,46 @@ int main() {
     printf("=== Carta 1 ===\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
+    printf("Cidade: %c\n", cidade1);
+    printf("Populaçao: %s\n", populacao1);
+    printf("Area: %c\n", area1);
+    printf("Pib: %s\n", pib1);
+    printf("pontos: %s\n", pontos1);
 
+
+
+
+
+      printf("Digite o estado da carta 2: ");
+    scanf(" %c", &estado2);
+
+    printf("Digite o Codigo da carta 2: ");
+    scanf(" %c", &codigo2);
+
+    printf("Digite a Cidade da carta 2: ");
+    scanf(" %s", &cidade2);
+
+    printf("Digite o tamanho da população da carta 2: ");
+    scanf(" %d", &populacao2);
+
+    printf("Digite a area da carta 2: ");
+    scanf(" %f", &area2);
+
+    printf("Digite o pib da carta 2: ");
+    scanf(" %f", &pib2);
+
+    printf("Digite o pontos turisticos da carta 2: ");
+    scanf(" %d", &pontos2);
+
+
+    printf("=== Carta 2 ===\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %c\n", cidade2);
+    printf("Populaçao: %s\n", populacao2);
+    printf("Area: %c\n", area2);
+    printf("Pib: %s\n", pib2);
+    printf("pontos: %s\n", pontos2);
+    
     return 0;
 }
